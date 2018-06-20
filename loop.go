b4b2b11984f84afee3068090fdf0ac135854ccd3
André Moraes <andrebq@gmail.com>
2018-06-19 21:35:12 -0300

    code clean-up

--- loop.go
+++ loop.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/andrebq/space-invaders/ces"
 	"github.com/andrebq/space-invaders/ces/input"
-	"github.com/sirupsen/logrus"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -26,33 +24,4 @@ func loopForever(ctx context.Context, win *sdl.Window, world *ces.World, input *
 			return ctx.Err()
 		}
 	}
-}
-
-func processEvents(win *sdl.Window) bool {
-	for ev := sdl.WaitEvent(); ev != nil; ev = sdl.PollEvent() {
-		switch ev := ev.(type) {
-		case *sdl.QuitEvent:
-			return true
-		case *sdl.WindowEvent:
-			if ev.Type == sdl.WINDOWEVENT_CLOSE &&
-				sameWindow(win, ev.WindowID) {
-				// since we only have one window
-				// if it is closed
-				// we can quit
-				return true
-			}
-		default:
-			logrus.WithField("event", fmt.Sprintf("%#v", ev)).Print()
-		}
-	}
-	return false
-}
-
-func sameWindow(win *sdl.Window, evWindowID uint32) bool {
-	winID, err := win.GetID()
-	if err != nil {
-		logrus.WithError(err).Error("unable to obtain ID from window")
-		return false
-	}
-	return winID == evWindowID
 }
